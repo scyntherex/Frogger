8f3419235c5470f76153acab7452357fe8943609
Rone <scyntherex@Rones-MacBook-Pro.local>
2017-10-26 17:43:52 -0700

    Added link of tutorial

--- src/Frogger.java
+++ src/Frogger.java
@@ -3,7 +3,8 @@ public class Frogger {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World!");
+		System.out.println("Hello World!"
+				+ "http://www.cis.upenn.edu/~cis110/13fa/lectures/examples/Frogger.java");
 	}
 
 }
